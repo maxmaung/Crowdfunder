d533de46d2ebb8f1484c81d16411965b72b95c49
Victor McLean <vmvictorvm@gmail.com>
2016-09-21 15:58:00 -0400

    create models, associations and controllers

--- Crowdfunder/app/models/project.rb
+++ Crowdfunder/app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+  has_many :pledges
+  belongs_to :owner_user, class_name: 'User', inverse_of: :owned_projects
+  has_many :users, through: :pledges
 end
---
+++ Crowdfunder/app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_many :owned_projects, class_name: 'Project', inverse_of: :owner_user
+  has_many :projects, through: :pledges
+  has_many :pledges
+
+  has_secure_password
+end
