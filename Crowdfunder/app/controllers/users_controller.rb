7795bd132c34c4a0e296114e21d9824ec9d32f99
Natalie Dodd <nataliedodd89@gmail.com>
2016-09-22 13:41:12 -0400

    fixed issue with saving user

--- Crowdfunder/app/controllers/users_controller.rb
+++ Crowdfunder/app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
    def create
      @user = User.new(user_params)
        if @user.save
-         session[:id] = user.id
          redirect_to root_path, alert: "Account sucessfully created!"
        else
          flash.now[:alert] = 'Oops!'
