3edc084a7fc8acb4b379d065f2fd416a2a1596f5
Natalie Dodd <nataliedodd89@gmail.com>
2016-09-22 07:22:20 -0400

    created views for user

--- Crowdfunder/config/routes.rb
+++ Crowdfunder/config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'projects#index'
-  
+
   resources :projects do
     resources :pledges
   end
+
+  resources :sessions,    only: [:new, :create, :destroy]
 
 end
